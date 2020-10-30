b5c91f1e190f0406a794fe146d0a6e74889c0340
chimingg <68934510+chimingg@users.noreply.github.com>
2020-10-31 02:09:59 +0700

    Add files via upload

---
+++  1.2 new.php
@@ -0,0 +1,10 @@
+https://classroom.github.com/a/YUlsGeoD
+ */
+printf("input your first number:");
+fscanf(STDIN, "%s", $first);
+printf("input your second number:");
+fscanf(STDIN, "%s", $second);
+if ($first>$second) printf ($first. " greater than ". $second);
+else if ($first==$second) printf($first. " equal to ". $second);
+else if ($first<$second) printf($first. " less than ". $second);
+else printf("invalid your menue number");
